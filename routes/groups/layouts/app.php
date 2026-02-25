de69d4f87952d618340ad8641b1dc24777afaf2a
fantastic0101 <austindev0101@gmail.com>
2026-02-25 15:47:45 -0500

    route fix

--- routes/groups/layouts/app.php
+++ routes/groups/layouts/app.php
@@ -4,4 +4,5 @@
 use App\Http\Controllers\Layouts\ApplicationController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', ApplicationController::class);
 Route::get('{view}', ApplicationController::class)->where('view', '(.*)');
